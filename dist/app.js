c2ed2e25ac3b40127821ce663e52162fc61e3fa3
predadorakrid <alexis.navarrete@netlogistik.local>
2021-06-17 14:46:15 -0500

    modulo finalizado

--- dist/app.js
+++ dist/app.js
@@ -1,35 +1,26 @@
 "use strict";
-function combine(input1, input2, resultType) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' && resultType === 'as-number') {
-        return result = +input1 + +input2;
-    }
-    else {
-        return result = input1.toString() + input2.toString();
-    }
-}
-function add(x, y) {
-    return x + y;
-}
-const combineAges = combine(30, 20, 'as-number');
-const combineNames = combine('Alexis', ' Navarrete', 'as-text');
-const combineNamesNumber = combine('10', ' 20', 'as-number');
-//let combineValues: Function; forma 1
-//let combineValues: () => number; forma 2
-let combineValues;
-combineValues = add;
-console.log(combineValues(1, 2));
-function addAndHandle(n1, n2, cb) {
-    const result = n1 + n2;
-    cb(result);
-}
-console.log(addAndHandle(1, 200, (result) => {
+const userName = 'Alexis';
+let age = 30;
+age = 29;
+var result;
+function add(a, b) {
+    result = a + b;
     return result;
-}));
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
 }
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log("Clicked!");
-});
+const add2 = (a, b) => {
+    return a + b;
+};
+const add3 = (a, b) => a + b;
+const printOutput = output => console.log(output);
+const add4 = (a, b = 1) => a + b;
+console.log(add4(10));
+const hobbies = ['nadar', 'correr'];
+const activeHobbies = ['bicicleta'];
+activeHobbies.push(...hobbies);
+const add5 = (...numbers) => {
+    return numbers.reduce((currResult, currValue) => {
+        return currResult + currValue;
+    }, 0);
+};
+console.log(add5(1, 2, 3, 4, 5.5));
+const [hobby1, hobby2] = hobbies;
--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,44 @@
-type Combinable = 'as-number' | 'as-text'
+const userName = 'Alexis';
 
-function combine(input1: number | string, input2: number | string, resultType: 'as-number'|'as-text'){
-    let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number' && resultType === 'as-number'){
-        return  result = +input1 + +input2;
-    }else{
-        return result = input1.toString() + input2.toString();
-    }
+let age = 30;
+age = 29;
+
+var result;
+
+function add(a:number, b: number){
+    result = a + b;
+    return result 
+}
+const add2 = (a:number, b:number) => {
+    return a+b
+};
+
+const add3 = (a:number, b:number) => a+b; //solo si tenemos una linea de código hace lo mismo que add y add2
+
+const printOutput: (a: number|string) => void = output => console.log(output);
+
+//podemos tener default values
+
+const add4 = (a:number, b:number = 1) => a+b;
+
+console.log(add4(10)); //nos da 11 pero los default parameters debe estar de derecha a izquierda obligatoriamente
+
+const hobbies = ['nadar', 'correr'];
+const activeHobbies = ['bicicleta']; //los arreglos son objetos y los objetos, referencias a memoria 
+
+
+activeHobbies.push(...hobbies); //al hacer esto estamos evitando tener la referencia, i.e
+
+const add5 = (...numbers: number[])  => {
+     
+    return numbers.reduce((currResult, currValue ) =>  {
+        return currResult + currValue
+    }, 0) ; //numbers.forEach equivalent
     
- 
- 
-}
+};
 
-function add(x: number, y: number){
-    return x+y;
-}
+console.log(add5(1,2,3,4,5.5))
 
-const combineAges = combine(30,20, 'as-number');
-const combineNames = combine('Alexis', ' Navarrete', 'as-text')
-const combineNamesNumber = combine('10', ' 20', 'as-number')
+const [hobby1, hobby2] = hobbies;
 
 
-
-//let combineValues: Function; forma 1
-//let combineValues: () => number; forma 2
-let combineValues: (a:number, b:number) => number;
-
-
-combineValues = add;
-
-console.log(combineValues(1,2));
-
-function addAndHandle(n1: number, n2: number, cb: (num:number) => void){
-    const result = n1 + n2;
-    cb(result);
-}
-
-console.log(addAndHandle(1,200, (result) => {
-    return result;
-}))
-
-function generateError(message:string, code:number){
-    throw {message: message, errorCode: code}
-}
-
-const button = document.querySelector('button')!;
-button.addEventListener('click', () => {
-    console.log("Clicked!");
-});
-
- 
