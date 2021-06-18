5e05e6bd20daa205fba0dce1a71a61ff3cbcaa43
predadorakrid <alexis.navarrete@netlogistik.local>
2021-06-18 09:28:07 -0500

    terminando módulo de POO

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,26 @@
 "use strict";
-const userName = 'Alexis';
-let age = 30;
-age = 29;
-var result;
-function add(a, b) {
-    result = a + b;
-    return result;
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    greet(phrase) {
+        console.log(phrase + this.name);
+    }
 }
-const add2 = (a, b) => {
-    return a + b;
-};
-const add3 = (a, b) => a + b;
-const printOutput = output => console.log(output);
-const add4 = (a, b = 1) => a + b;
-console.log(add4(10));
-const hobbies = ['nadar', 'correr'];
-const activeHobbies = ['bicicleta'];
-activeHobbies.push(...hobbies);
-const add5 = (...numbers) => {
-    return numbers.reduce((currResult, currValue) => {
-        return currResult + currValue;
-    }, 0);
-};
-console.log(add5(1, 2, 3, 4, 5.5));
-const [hobby1, hobby2] = hobbies;
+class Student {
+    constructor(n) {
+        this.age = 30;
+        if (n) {
+            this.name = n;
+        }
+        else {
+            this.name = "";
+            console.log("hi");
+        }
+    }
+    greet() {
+        console.log("Hola mi nombre es: " + this.name);
+    }
+}
+const student1 = new Student("Alexis");
+student1.greet();
--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,57 @@
-const userName = 'Alexis';
-
-let age = 30;
-age = 29;
-
-var result;
-
-function add(a:number, b: number){
-    result = a + b;
-    return result 
+// solo las estructuras
+interface Greetable   {
+    name: string;
+    greet(phrase: string): void;
 }
-const add2 = (a:number, b:number) => {
-    return a+b
-};
 
-const add3 = (a:number, b:number) => a+b; //solo si tenemos una linea de código hace lo mismo que add y add2
+class Person implements Greetable{
+    name:string;
 
-const printOutput: (a: number|string) => void = output => console.log(output);
+    constructor(name:string){
+        this.name = name;
+    }
+    greet(phrase:string){
+        console.log(phrase + this.name);
+    }
 
-//podemos tener default values
-
-const add4 = (a:number, b:number = 1) => a+b;
-
-console.log(add4(10)); //nos da 11 pero los default parameters debe estar de derecha a izquierda obligatoriamente
-
-const hobbies = ['nadar', 'correr'];
-const activeHobbies = ['bicicleta']; //los arreglos son objetos y los objetos, referencias a memoria 
+}
 
 
-activeHobbies.push(...hobbies); //al hacer esto estamos evitando tener la referencia, i.e
-
-const add5 = (...numbers: number[])  => {
-     
-    return numbers.reduce((currResult, currValue ) =>  {
-        return currResult + currValue
-    }, 0) ; //numbers.forEach equivalent
-    
-};
-
-console.log(add5(1,2,3,4,5.5))
-
-const [hobby1, hobby2] = hobbies;
 
 
+
+
+
+//interfaces con atributos y métodos opcionales de implementar
+interface Named{  
+    name:string; 
+    outPutName?: string;
+}
+
+interface Greetable extends Named{
+    greet(phrase:string): void;
+
+
+}
+
+class Student implements Greetable{
+    readonly name:string;
+    age = 30;
+    constructor(n?:string){
+        if(n){
+            this.name = n;
+        }else{
+            this.name = "";
+            console.log("hi")
+            
+        }
+            
+    }
+    greet(): void{
+        console.log("Hola mi nombre es: " + this.name);
+    }
+}
+
+
+const student1 = new Student("Alexis")
+student1.greet()
